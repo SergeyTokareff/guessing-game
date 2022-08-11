a4d00806a8326314fdc587d17c79a51d7ba0ea20
SergeyTokareff <sergey.v.tokareff@gmail.com>
2022-08-11 16:59:05 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
 
-    }
+	guess() {
+		this.num = Math.ceil((this.min + this.max) / 2);
+		return this.num;
+	}
 
-    guess() {
+	lower() {
+		this.max = this.num;
+		this.guess();
+	}
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = this.num;
+		this.guess();
+	}
 }
 
 module.exports = GuessingGame;
